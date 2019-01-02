fa22c966c97cea9ec139e9b86d960799676ead15
Rodrigo Klaes <rodrigoklaes@gmail.com>
2019-01-01 23:17:47 -0200

    Back-end

---
+++ front/src/helpers/if.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default props => {
+  if (props.test) { return props.children } else { return false }
+}
--- front/src/todo/todo.jsx
+++ front/src/todo/todo.jsx
@@ -1,11 +1,34 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
-export default class Todo extends Component{
-    render(){
-        return (
-            <div>
-                <h1>Todo</h1>
-            </div>
-        )
-    }
+export default class Todo extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { description: '', list: [] }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({ ...this.state, description: e.target.value })
+  }
+
+  handleAdd () {
+    console.log(this.state.description)
+  }
+
+  render () {
+    return (
+      <div>
+        <PageHeader name='Tarefas' small='cadastro' />
+        <TodoForm
+          handleChange={this.handleChange}
+          handleAdd={this.handleAdd} />
+        <TodoList />
+      </div>
+    )
+  }
 }
